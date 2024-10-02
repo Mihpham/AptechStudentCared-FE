945d30aaf8b496ab4708b976803decbd2e86402f
DoanQuang <doanquang3645@gmail.com>
2024-10-02 14:57:15 +0700

    update, delete, regenerate schedule call api

--- src/app/core/services/admin/schedules.service.ts
+++ src/app/core/services/admin/schedules.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserEnviroment } from 'src/app/environments/environment';
+import { CreateScheduleRequest } from 'src/app/features/admin-management/model/schedules/create-schedule-request.model';
 import { ScheduleRequest } from 'src/app/features/admin-management/model/schedules/schedule-request.model';
+import { Schedule } from 'src/app/features/admin-management/model/schedules/schedules.model';
 
 @Injectable({
   providedIn: 'root',
@@ -21,11 +23,24 @@ export class ScheduleService {
   createSchedule(
     classId: number,
     subjectId: number,
-    request: ScheduleRequest
+    request: CreateScheduleRequest
   ): Observable<any[]> {
     return this.http.post<any[]>(
       `${this.baseUrl}/create/class/${classId}/subject/${subjectId}`,
       request
     );
   }
+
+  regenerateSchedules(classId: number, subjectId: number, request: ScheduleRequest): Observable<Schedule[]> {
+    return this.http.put<Schedule[]>(`${this.baseUrl}/class/${classId}/subject/${subjectId}`, request);
+  }
+
+  updateScheduleById(id: number, schedule: Schedule): Observable<Schedule> {
+    return this.http.put<Schedule>(`${this.baseUrl}/${id}`, schedule);
+  }
+
+  deleteScheduleById(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/${id}`);
+  }  
+
 }
