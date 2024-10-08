d91528e5c68add7b956df770b64d8e5a31cee0e7
trongas <trongdinh07@gmail.com>
2024-10-08 21:43:52 +0700

    dashboard-full

--- src/app/features/sro-management/sro-management.module.ts
+++ src/app/features/sro-management/sro-management.module.ts
@@ -2,7 +2,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SroManagementRoutingModule } from './sro-management-routing.module';
 import { SroComponent } from './sro.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -10,7 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [SroComponent, DashboardComponent],
+  declarations: [SroComponent],
   imports: [
     CommonModule,
     FormsModule,
